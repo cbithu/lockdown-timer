f937f87b78503ee7684f2890c29fa3071ebeaa20
Chinmay Bithu <cbithu@deloitte.com>
2020-04-01 13:54:54 +0530

    Updates

---
+++ precache-manifest.dda7f8f17cda2514308ec1f01532b65c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ede8c7ad98ced7ceb0b370126c0fda5",
+    "url": "/lockdown-timer/index.html"
+  },
+  {
+    "revision": "c842712fbbcd978db38d",
+    "url": "/lockdown-timer/static/css/main.82344e47.chunk.css"
+  },
+  {
+    "revision": "10359c6ce3d036b9105a",
+    "url": "/lockdown-timer/static/js/2.2a00dd1d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lockdown-timer/static/js/2.2a00dd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c842712fbbcd978db38d",
+    "url": "/lockdown-timer/static/js/main.b11dde93.chunk.js"
+  },
+  {
+    "revision": "835d2ce0831f237f83b1",
+    "url": "/lockdown-timer/static/js/runtime-main.78dbd4d6.js"
+  }
+]);
